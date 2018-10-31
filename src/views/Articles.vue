36747ffb622e47a69a3048b4b1725b6be7d678f2
sofron80 <sofron80@mail.ru>
2018-10-31 14:04:08 +0600

    Create  article.vue

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,13 +1,40 @@
 <template lang="pug">
   div(md10 offset-md1)
-    app-articles
+    v-layout
+      v-flex(xs12 sm10 offset-sm1 wrap)
+        v-layout(justify-center mt-5 v-if="isLoading")
+          v-progress-circular(
+          justify-center
+          :size="70"
+          :width="7"
+          color="primary"
+          indeterminate
+          )
+        div(v-else)
+          v-card(v-for="(item, i) in getArticles" :key="i")
+            v-card-title
+            span {{item.title}}
+            span {{item.text}}
 </template>
 
 <script>
-import AppArticles from '@/components/AppArticles'
 export default {
-  components:{
-    AppArticles
+  data(){
+    return {
+    }
+  },
+  computed:{
+    getArticles(){
+      let a = this.$store.getters.getArticles
+      return a
+    },
+    isLoading(){
+      return this.$store.getters.getLoading
+    }
+  }
+  ,
+  created(){
+    this.$store.dispatch('loadArticles')
   }
 }
 </script>
